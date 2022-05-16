bc2e558d4589938da36ebe4ff989e71f2a4de806
Deepthi-SP <100423337+Deepthi-SP@users.noreply.github.com>
2022-05-16 16:20:25 +0530

    Add files via upload

--- topology.c
+++ topology.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int a[6][6],n,visited[10],count=0;
+int a[5][5],n,visited[10],count=0,k;
+int topo[10];
 void dfs(int v)
 {
 int w;
@@ -9,13 +10,14 @@ int w;
   if(visited[w]==0 && a[v][w]==1)
     dfs(w);
     printf("%d\t",v);
+    topo[k++]=v;
 }  
 void main()
 {
   int i,j;
   printf("Enter number of vertices\n");
   scanf("%d",&n);
-  printf("Enter the matrix\n");
+  printf("Enter the matrix(DAG)-----\n");
   for(i=0;i<n;i++)
       for(j=0;j<n;j++)
         scanf("%d",&a[i][j]);
@@ -26,12 +28,18 @@ void main()
      printf("%d\t",a[i][j]);
      printf("\n");
   }
-   printf("\n-------DFS Traversal---------------\n");
+  printf("\n--------------DFS Traversal---------------\n");
   for(i=0;i<n;i++)
   visited[i]=0;
-  dfs(0);
-  if(count==n)
+  for(i=0;i<n;i++)
+  if(visited[i]==0)
+  dfs(i);
+   if(count==n)
   printf("\nGraph is connected\n");
   else 
   printf("\nGraph is not connectd\n");
+  printf("\n-----------topology sorting---------------\n");
+  for(i=n-1;i>=0;i--)
+  printf("%d\t",topo[i]);
+ 
 }
